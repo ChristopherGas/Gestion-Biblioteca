b96123367142de9462a7f3862335fc697c78e951
itzelMirand <itzelmiranda11@hotmail.com>
2020-12-23 14:26:27 -0600

    model-bean-libro

--- GestionBiblioteca/src/main/java/com/gestion/bibliotecaModel/Libros.java
+++ GestionBiblioteca/src/main/java/com/gestion/bibliotecaModel/Libros.java
@@ -1,5 +1,97 @@
 package com.gestion.bibliotecaModel;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name = "Libros")
 public class Libros {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	@Column(name = "id_libro")
+	private int idLibro;
+	
+	@Column(name = "nombre_libro",length = 30, nullable = false)
+	private String nombreLibro;
+	
+	@Column(name = "fecha_lanzamiento_libro", length = 10, nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date fechaLanzamientoLibro;
+	
+	@Column(name = "editoral_libro", length = 30, nullable = false)
+	private String editoralLibro;
+	
+	@Column(name = "no_paginas_libro", nullable = false)
+	private int noPaginasLibro;
+	
+	@Column(name = "existencia_libro", nullable = false)
+	private int existenciaLibro;
+
+	public Libros() {
+		super();
+	}
+
+	public Libros(int idLibro) {
+		super();
+		this.idLibro = idLibro;
+	}
+
+	public int getIdLibro() {
+		return idLibro;
+	}
+
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+
+	public String getNombreLibro() {
+		return nombreLibro;
+	}
+
+	public void setNombreLibro(String nombreLibro) {
+		this.nombreLibro = nombreLibro;
+	}
+
+	public Date getFechaLanzamientoLibro() {
+		return fechaLanzamientoLibro;
+	}
+
+	public void setFechaLanzamientoLibro(Date fechaLanzamientoLibro) {
+		this.fechaLanzamientoLibro = fechaLanzamientoLibro;
+	}
+
+	public String getEditoralLibro() {
+		return editoralLibro;
+	}
+
+	public void setEditoralLibro(String editoralLibro) {
+		this.editoralLibro = editoralLibro;
+	}
+
+	public int getNoPaginasLibro() {
+		return noPaginasLibro;
+	}
+
+	public void setNoPaginasLibro(int noPaginasLibro) {
+		this.noPaginasLibro = noPaginasLibro;
+	}
+
+	public int getExistenciaLibro() {
+		return existenciaLibro;
+	}
+
+	public void setExistenciaLibro(int existenciaLibro) {
+		this.existenciaLibro = existenciaLibro;
+	}
+	
 }
--- GestionBiblioteca/src/main/java/com/gestion/bibliotecaModelBean/LibrosBean.java
+++ GestionBiblioteca/src/main/java/com/gestion/bibliotecaModelBean/LibrosBean.java
@@ -1,5 +1,106 @@
 package com.gestion.bibliotecaModelBean;
 
-public class LibrosBean {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+public class LibrosBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private int idLibro;
+	
+	@Size(max = 30)
+	@NotNull
+	private String nombreLibro;
+	
+	@Size(max = 10)
+	@NotNull
+	private Date fechaLanzamientoLibro;
+	
+	@Size(max = 30)
+	@NotNull
+	private String editoralLibro;
+	
+	@NotNull
+	private int noPaginasLibro;
+	
+	@NotNull
+	private int existenciaLibro;
+
+	
+	
+	public LibrosBean() {
+		super();
+	}
+
+	public LibrosBean(int idLibro) {
+		super();
+		this.idLibro = idLibro;
+	}
+
+	
+	
+	public int getIdLibro() {
+		return idLibro;
+	}
+
+	public void setIdLibro(int idLibro) {
+		this.idLibro = idLibro;
+	}
+
+	public String getNombreLibro() {
+		return nombreLibro;
+	}
+
+	public void setNombreLibro(String nombreLibro) {
+		this.nombreLibro = nombreLibro;
+	}
+
+	public Date getFechaLanzamientoLibro() {
+		return fechaLanzamientoLibro;
+	}
+
+	public void setFechaLanzamientoLibro(Date fechaLanzamientoLibro) {
+		this.fechaLanzamientoLibro = fechaLanzamientoLibro;
+	}
+
+	public String getEditoralLibro() {
+		return editoralLibro;
+	}
+
+	public void setEditoralLibro(String editoralLibro) {
+		this.editoralLibro = editoralLibro;
+	}
+
+	public int getNoPaginasLibro() {
+		return noPaginasLibro;
+	}
+
+	public void setNoPaginasLibro(int noPaginasLibro) {
+		this.noPaginasLibro = noPaginasLibro;
+	}
+
+	public int getExistenciaLibro() {
+		return existenciaLibro;
+	}
+
+	public void setExistenciaLibro(int existenciaLibro) {
+		this.existenciaLibro = existenciaLibro;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "LibrosBean [idLibro=" + idLibro + ", nombreLibro=" + nombreLibro + ", fechaLanzamientoLibro="
+				+ fechaLanzamientoLibro + ", editoralLibro=" + editoralLibro + ", noPaginasLibro=" + noPaginasLibro
+				+ ", existenciaLibro=" + existenciaLibro + "]";
+	}
+	
 }
