b4fafb5af3a4690323188c2046ada995fef845f8
lost-dir <carloscamacho607@gmail.com>
2020-12-23 14:45:09 -0600

    clase usuario y bean

---
+++ GestionBiblioteca/src/main/java/com/gestion/bibliotecaModel/Usuario.java
@@ -0,0 +1,53 @@
+package com.gestion.bibliotecaModel;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario")
+	private int idUsuario;
+	@Column(name = "nombre_usuario", nullable = false, length = 50)
+	private String nombreUsuario;
+	@Column(name = "direccion_usuario", nullable = false, length = 100)
+	private String direccionUsuario;
+	@Column(name = "sexo_usuario", nullable = false, length = 5)
+	private String sexoUsuario;
+	
+	public Usuario(){}
+	public Usuario(int idUsuario){
+		this.idUsuario = idUsuario;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getDireccionUsuario() {
+		return direccionUsuario;
+	}
+	public void setDireccionUsuario(String direccionUsuario) {
+		this.direccionUsuario = direccionUsuario;
+	}
+	public String getSexoUsuario() {
+		return sexoUsuario;
+	}
+	public void setSexoUsuario(String sexoUsuario) {
+		this.sexoUsuario = sexoUsuario;
+	}
+
+}
---
+++ GestionBiblioteca/src/main/java/com/gestion/bibliotecaModelBean/UsuarioBean.java
@@ -0,0 +1,60 @@
+package com.gestion.bibliotecaModelBean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+public class UsuarioBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Column(name = "id_usuario")
+	private int idUsuario;
+	@NotNull
+	@Size(max = 50)
+	@Column(name = "nombre_usuario")
+	private String nombreUsuario;
+	@NotNull
+	@Size(max = 100)
+	@Column(name = "direccion_usuario")
+	private String direccionUsuario;
+	@NotNull
+	@Size(max = 5)
+	@Column(name = "sexo_usuario")
+	private String sexoUsuario;
+	public UsuarioBean() {
+	}
+	public UsuarioBean(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+	public String getDireccionUsuario() {
+		return direccionUsuario;
+	}
+	public void setDireccionUsuario(String direccionUsuario) {
+		this.direccionUsuario = direccionUsuario;
+	}
+	public String getSexoUsuario() {
+		return sexoUsuario;
+	}
+	public void setSexoUsuario(String sexoUsuario) {
+		this.sexoUsuario = sexoUsuario;
+	}
+	
+}
